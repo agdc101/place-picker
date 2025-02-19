13a0dfed93a1759db1cfebc86f4a1987df64b25f
agdc101 <agdc101@gmail.com>
2025-02-19 15:34:57 +0000

    progress bar

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,8 +1,21 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+    const [remainingTime, setRemainingTime] = useState(2000);
 
     useEffect(() => {
+        console.log("Remaining Time Interval", remainingTime);
+        const interval = setInterval(() => {
+            setRemainingTime((prevTime) => prevTime - 10);
+        }, 10);
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, [])
+
+    useEffect(() => {
+        console.log("Remaining Time timer", remainingTime);
         const timer = setTimeout(() => {
             onConfirm();
         }, 2000);
@@ -11,20 +24,21 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
             clearTimeout(timer);
         }
 
-    }, []);
+    }, [onConfirm]);
 
     return (
         <div id="delete-confirmation">
-          <h2>Are you sure?</h2>
-          <p>Do you really want to remove this place?</p>
-          <div id="confirmation-actions">
-            <button onClick={onCancel} className="button-text">
-              No
-            </button>
-            <button onClick={onConfirm} className="button">
-              Yes
-            </button>
-          </div>
+            <h2>Are you sure?</h2>
+            <p>Do you really want to remove this place?</p>
+            <div id="confirmation-actions">
+                <button onClick={onCancel} className="button-text">
+                  No
+                </button>
+                <button onClick={onConfirm} className="button">
+                  Yes
+                </button>
+            </div>
+            <progress value={remainingTime} max={2000} />
         </div>
     );
 }
